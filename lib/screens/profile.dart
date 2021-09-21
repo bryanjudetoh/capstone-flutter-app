7a31272f910c610a2b20cb71598b770b1c7909ab
laurentyee <yeeqixuan1@gmail.com>
2021-09-21 16:02:14 +0800

    profile header

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -10,6 +10,13 @@ class ProfileScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const captionTextStyle = TextStyle(
+      fontFamily: 'SF Pro Display',
+      fontSize: 15.0,
+      height: 1.25,
+      color: kDarkGrey,
+    );
+
     return Container(
       margin: EdgeInsets.only(left: 35.0, right: 35.0, top: 50.0),
       child: Column(
@@ -29,8 +36,101 @@ class ProfileScreenBody extends StatelessWidget {
               ]
           ),
           SizedBox(
-            height: 150,
+            height: 20,
           ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                width: 3,
+                color: Colors.white,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/images/profile-img.png'),
+                      height: 80,
+                      width: 80,
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Behbat',
+                          textAlign: TextAlign.left,
+                          style: bodyTextStyleBold,
+                        ),
+                        Text(
+                          '77th school',
+                          textAlign: TextAlign.left,
+                          style: captionTextStyle,
+                        ),
+                        Text(
+                          '12th grade',
+                          textAlign: TextAlign.left,
+                          style: captionTextStyle,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('assets/images/elixir.png'),
+                          height: 40,
+                          width: 40,
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              'Elixirs',
+                              style: captionTextStyle,
+                            ),
+                            Text(
+                              '1/100',
+                              style: bodyTextStyleBold,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Friends',
+                          style: captionTextStyle,
+                        ),
+                        Text(
+                          '52',
+                          style: bodyTextStyleBold,
+                        ),
+                      ]
+                    )
+                  ]
+                )
+              ]
+            )
+          )
         ],
       ),
     );
