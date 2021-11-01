e522df3ae7eb90b4d2c20385b2e7980012215d5c
bryanjudetoh <bryan.judetoh@gmail.com>
2021-11-02 03:18:06 +0800

    added functionality to add pictures to create post

--- lib/screens/create-post.dart
+++ lib/screens/create-post.dart
@@ -9,7 +9,7 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
-import 'package:youthapp/widgets/alert-popup.dart';
+import 'package:youthapp/utilities/securestorage.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 import 'package:youthapp/utilities/authheader-interceptor.dart';
 import 'package:youthapp/utilities/refreshtoken-interceptor.dart';
@@ -19,12 +19,13 @@ import '../constants.dart';
 class CreatePostScreen extends StatefulWidget {
   CreatePostScreen({Key? key}) : super(key: key);
 
-  final http = InterceptedHttp.build(
+  final intHttp = InterceptedHttp.build(
     interceptors: [
       AuthHeaderInterceptor(),
     ],
     retryPolicy: RefreshTokenRetryPolicy(),
   );
+  final SecureStorage secureStorage = SecureStorage();
 
   @override
   _CreatePostScreenState createState() => _CreatePostScreenState();
@@ -43,7 +44,7 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
+        child: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
           child: Column(
             children: <Widget>[
@@ -81,7 +82,7 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
                       onPressed: () async {
                         String message = '';
                         try {
-                          message = await doCreatePost(this.contentController.text);
+                          message = await doCreatePost(this.contentController.text, user);
                           Navigator.of(context).pushNamedAndRemoveUntil('/init-home', (route) => false);
                         }
                         on Exception catch (err) {
@@ -137,48 +138,59 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
                       validator: RequiredValidator(errorText: "* Required"),
                     ),
                     SizedBox(height: 10,),
-                    // Align(
-                    //   alignment: Alignment.centerRight,
-                    //   child: Container(
-                    //     padding: EdgeInsets.symmetric(horizontal: 5),
-                    //     decoration: BoxDecoration(
-                    //       color: kLightBlue,
-                    //       borderRadius: BorderRadius.circular(30),
-                    //     ),
-                    //     child: TextButton(
-                    //       style: TextButton.styleFrom(
-                    //           textStyle: TextStyle(
-                    //             fontFamily: 'SF Display Pro',
-                    //             fontWeight: FontWeight.bold,
-                    //             fontSize: 16.0,
-                    //             color: Colors.white,
-                    //           )
-                    //       ),
-                    //       child: Row(
-                    //         mainAxisSize: MainAxisSize.min,
-                    //         children: <Widget>[
-                    //           Icon(Icons.add, color: Colors.white,),
-                    //           SizedBox(width: 5,),
-                    //           Icon(Icons.camera_alt, color: Colors.white,),
-                    //         ],
-                    //       ),
-                    //       onPressed: () async {
-                    //         File? temp = await chooseProfilePicture(user);
-                    //         if (temp != null) {
-                    //           setState(() {
-                    //             this.uploadedMediaContent.add(temp);
-                    //           });
-                    //         }
-                    //       },
-                    //     ),
-                    //   ),
-                    // ),
-                    // Row(
-                    //   children: displayUploadedImages(),
-                    // ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 5),
+                        decoration: BoxDecoration(
+                          color: kLightBlue,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              textStyle: TextStyle(
+                                fontFamily: 'SF Display Pro',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                                color: Colors.white,
+                              )
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Icon(Icons.add, color: Colors.white,),
+                              SizedBox(width: 5,),
+                              Icon(Icons.camera_alt, color: Colors.white,),
+                            ],
+                          ),
+                          onPressed: () async {
+                            if (this.uploadedMediaContent.length < 9) {
+                              File? temp = await choosePictureFromGallery(user);
+                              if (temp != null) {
+                                setState(() {
+                                  this.uploadedMediaContent.add(temp);
+                                });
+                              }
+                            }
+                            else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      'Cannot add more than 9 photos',
+                                      style: bodyTextStyle,
+                                    ),
+                                    duration: const Duration(seconds: 1),
+                                  )
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ),
                   ],
                 )
               ),
+              displayUploadImages(),
             ],
           ),
         ),
@@ -186,36 +198,84 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
     );
   }
 
-  Future<String> doCreatePost(String content) async {
-    var response = await widget.http.post(
-      Uri.parse('https://eq-lab-dev.me/api/social-media/mp/post'),
-      body: jsonEncode(<String, String> {
-        "content": content,
-      })
-    );
+  Future<String> doCreatePost(String content, User user) async {
+    if (_formkey.currentState!.validate()) {
+      var response = await widget.intHttp.post(
+          Uri.parse('https://eq-lab-dev.me/api/social-media/mp/post'),
+          body: jsonEncode(<String, String> {
+            "content": content,
+          })
+      );
 
-    if (response.statusCode == 201) {
-      var responseBody = jsonDecode(response.body);
-      print(responseBody);
-      return 'Successfully created post';
+      if (response.statusCode == 201) {
+        Map<String, dynamic> responseBody = jsonDecode(response.body);
+        print(responseBody);
+        String postId = responseBody['postId'];
+        if (this.uploadedMediaContent.isNotEmpty) {
+          try {
+            String message = await uploadPicture(user, postId);
+            print(message);
+          }
+          on Exception catch (err) {
+            print(formatExceptionMessage(err.toString()));
+          }
+        }
+        return 'Successfully created post';
+      }
+      else {
+        var result = jsonDecode(response.body);
+        print(result);
+        throw Exception('A problem occured while creating your post');
+      }
     }
     else {
-      var result = jsonDecode(response.body);
-      print(result);
-      throw Exception('A problem occured while creating your post');
+      throw Exception('Validation Error');
     }
   }
-
-  List<Widget> displayUploadedImages() {
-    List<Widget> list = [];
-    for (File imageUrl in this.uploadedMediaContent) {
-      list.add(Image.file(imageUrl, height: 120, width: 120,));
-      list.add(SizedBox(width: 10,));
-    }
-    return list;
+  
+  Widget displayUploadImages() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      primary: false,
+      padding: EdgeInsets.all(5),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+      ),
+      itemCount: this.uploadedMediaContent.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsets.all(5.0),
+          child: Stack(
+            children: [
+              Image.file(
+                this.uploadedMediaContent[index],
+                height: MediaQuery.of(context).size.width*0.3,
+                width: MediaQuery.of(context).size.width*0.3,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  padding: EdgeInsets.all(3),
+                  constraints: BoxConstraints(),
+                  icon: Icon(Icons.cancel),
+                  onPressed: () {
+                    setState(() {
+                      this.uploadedMediaContent.removeAt(index);
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
   }
 
-  Future<File?> chooseProfilePicture(User user) async {
+  Future<File?> choosePictureFromGallery(User user) async {
     XFile? _xfile = await _picker.pickImage(source: ImageSource.gallery);
     if (_xfile == null) {
       return null;
@@ -223,40 +283,40 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
     return File(_xfile.path);
   }
 
-  Future<String?> uploadPicture(File? imageFile, User user) async {
-    if (imageFile != null) {
-      var stream = new http.ByteStream(imageFile.openRead());
-      stream.cast();
-      var length = await imageFile.length();
-      var request = http.MultipartRequest("POST", Uri.parse("https://eq-lab-dev.me/upload/image/profile-pic"));
-      var multipartFile = new http.MultipartFile('image', stream, length,
-          filename: basename(imageFile.path));
-      request.fields.addAll({
-        'platform': 'mp',
-        'userId': user.userId
-      });
-      request.files.add(multipartFile);
-
-      try {
-        final http.StreamedResponse response = await request.send();
-
-        if (response.statusCode == 200) {
-          var responseData = await response.stream.bytesToString();
-          Map map = jsonDecode(responseData);
-          return map['key'];
-        } else {
-          var responseData = await response.stream.bytesToString();
-          print(responseData);
-          throw Exception('Error faced');
-        }
-      }
-      on Exception catch (err) {
-        showDialog(
-            context: this.context,
-            builder: (BuildContext context) {
-              return AlertPopup(title: "Error", desc: formatExceptionMessage(err.toString()),);
-            });
-      }
+  Future<String> uploadPicture(User user, String postId) async {
+    final String accessToken = await widget.secureStorage.readSecureData('accessToken');
+    var request = http.MultipartRequest("POST", Uri.parse("https://eq-lab-dev.me/upload/image/post-pic"));
+    request.headers.addAll(<String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $accessToken',
+    });
+    for (File image in this.uploadedMediaContent) {
+      request.files.add(await prepareMultipartFile(image));
     }
+    request.fields.addAll({
+      'platform': 'mp',
+      'postId': postId,
+    });
+    final http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      var responseData = await response.stream.bytesToString();
+      Map<String, dynamic> map = jsonDecode(responseData);
+      print(map);
+      return 'Successfully uploaded post pictures';
+    } else {
+      var responseData = await response.stream.bytesToString();
+      print(responseData);
+      throw Exception('Error faced while uploading post pictures');
+    }
+  }
+
+  Future<http.MultipartFile> prepareMultipartFile(File imageFile) async {
+    var stream = new http.ByteStream(imageFile.openRead());
+    stream.cast();
+    var length = await imageFile.length();
+    var multipartFile = new http.MultipartFile('images', stream, length,
+        filename: basename(imageFile.path));
+    return multipartFile;
   }
 }
