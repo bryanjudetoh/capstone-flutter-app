5a9d79aa7dcf4d4dae4f51da7eeb71f8e750be01
bryanjudetoh <bryan.judetoh@gmail.com>
2021-11-18 01:44:06 +0800

    minor update to rate activity button on notifs

--- lib/screens/notification-details.dart
+++ lib/screens/notification-details.dart
@@ -349,7 +349,7 @@ class _NotificationDetailsScreenState extends State<NotificationDetailsScreen> {
           ),
         Text('${widget.notification.content}', style: bodyTextStyle,),
         SizedBox(width: double.infinity,),
-        if (widget.notification.activity != null)
+        if (widget.notification.isSystemTriggered)
           Align(
             alignment: Alignment.center,
             child: Column(
